17799fb18a2f712f663771689745caa30cc22876
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-03-25 00:59:01 +0900

    [front/create] 홈 nav의 html,css 생성

--- front/routes/show/program/programController.js
+++ front/routes/show/program/programController.js
@@ -1,8 +1,8 @@
 exports.main = (req,res, next) =>{
-    res.send('/쇼/프로그램 메인')
+    // res.send('/쇼/프로그램 메인')
     next()
 }
 
 exports.calendar = (req,res)=>{
-
+    res.render('show/showCalendar')
 }
