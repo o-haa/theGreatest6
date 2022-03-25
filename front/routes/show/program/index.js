66c75392cf34ea676fff45a477d90f454812cd2f
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-03-25 12:32:22 +0900

    [front\create] showView, showList, showGrid html과 라우터 생성&작성중

--- front/routes/show/program/index.js
+++ front/routes/show/program/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const programController = require('./programController')
 
 router.use('/', programController.main)
-router.get('/calendar', programController.calendar) //버튼 클릭 시 달력 팝업
-router.get('/ticketopen',) //티켓 예매까지만! 결제기능은 payment로 빠짐   
-router.get('/test',programController.test) //티켓 예매까지만! 결제기능은 payment로 빠짐   
+router.get('/calendar',programController.calendar) //버튼 클릭 시 달력 팝업
+router.get('/showlist',programController.showList) //리스트 페이지   
+router.get('/showgrid',programController.showGrid) //그리드 페이지   
 router.get('/showview',programController.showView) //상세페이지   
 
+router.get('/ticketopen',) //티켓 예매까지만! 결제기능은 payment로 빠짐   
+router.get('/test',programController.test)//my cal test용도
 module.exports = router
