9505f9f1d9bbdd06fd0c2dd5884ea1b615caf9f1
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-03-25 11:35:21 +0900

    [front/create] showView 라우터 파일만 생성함

--- front/routes/show/program/index.js
+++ front/routes/show/program/index.js
@@ -6,5 +6,6 @@ router.use('/', programController.main)
 router.get('/calendar', programController.calendar) //버튼 클릭 시 달력 팝업
 router.get('/ticketopen',) //티켓 예매까지만! 결제기능은 payment로 빠짐   
 router.get('/test',programController.test) //티켓 예매까지만! 결제기능은 payment로 빠짐   
+router.get('/showview',programController.showView) //상세페이지   
 
 module.exports = router
--- front/routes/show/program/programController.js
+++ front/routes/show/program/programController.js
@@ -6,6 +6,10 @@ exports.main = (req,res, next) =>{
 exports.calendar = (req,res)=>{
     res.render('./show/showCalendar')
 }
+//리스트 보여주는 기능
+exports.showView = (req,res)=>{
+    res.render('./show/showView')
+}
 
 //티켓 예매까지만! 결제기능은 payment로 빠짐   
 exports.ticketopen = (req,res)=>{
