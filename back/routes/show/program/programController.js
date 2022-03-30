5b632ec345fd9e8f29e7ae43eb80596246094149
hancoco <lhj12ps@gmail.com>
2022-03-30 23:24:27 +0900

    [back/create] show write sql 연결 완료

--- back/routes/show/program/programController.js
+++ back/routes/show/program/programController.js
@@ -12,25 +12,44 @@ exports.main = (req,res, next) =>{
 
 exports.showWrite = async (req,res)=>{
     console.log('back / showWrite 라우터 접속!')
+    const today = new Date()
+    const thisYear = today.getFullYear()
 
-    const {category, xrated, title, ticketMonth, ticketDate, ticketHour, place, showMain, showSub, showDirector, showCompany} = req.body
-    
-    const sql = `INSERT INTO shows(show_title,show_category_idx,show_xrated,show_company,show_director,show_like,show_open_flag,show_content) VALUES (?,?,?,?,?,10,20,'40');`
+    const {category, xrated, title, place, showCast1, showCast2, showDirector,showCompany,showContent,ticketMonth,ticketDate,ticketHour,showMonth,showDate,showHour} = req.body
 
-    const prepare = [title,category,xrated,showCompany,showDirector]
-    
     try{
-        const [result] = await pool.execute(sql,prepare)
-        const response = {
-            result,
+        const sqlShow = `INSERT INTO shows(
+            show_title,
+            show_category_idx,
+            show_xrated,
+            show_company,
+            show_director,
+            show_like,
+            show_content,
+            show_open_flag
+        )VALUES(?,?,?,?,?,'0',?,'0')`
+        const prespareShow = [title,category, xrated,showCompany,showDirector,showContent]
+        const [resultShow] = await pool.execute(sqlShow,prespareShow)
+    
+        const timestamp = `${thisYear}-${ticketMonth}-${ticketDate} ${ticketHour}:00`
+        const newIdx = resultShow.insertId
+
+        const prespareOption = [newIdx, timestamp, place, showCast1, showCast2]
+        const sqlOption = `INSERT
+        INTO s_option(shows_idx, show_date, show_place, show_cast1, show_cast2)
+        VALUES (?,?,?,?,?)`
+        const [resultOption] = await pool.execute(sqlOption,prespareOption)
+ 
+        response = {
+            resultShow,
+            resultOption,
             error:0,
         }
         res.json(response)
     }
     catch(e){
-        console.log("showWrite 에러발생")
+        console.log(e)
     }
-
 }
 
 exports.showList = async (req,res)=>{
