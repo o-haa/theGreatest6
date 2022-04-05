a6b92a376eab305c3b70e6d93b590c4e2972c135
jihyun kim <jihyunivy@gmail.com>
2022-04-05 17:51:44 +0900

    done community comment

--- front/public/script/board/communityList.js
+++ front/public/script/board/communityList.js
@@ -9,7 +9,7 @@ async function init() {
     const { user } = response1.data.result;
     const user_nickname = user.user_nickname;
 
-    const check = document.querySelectorAll('#category ul li input');
+    const checks = document.querySelectorAll('#category ul li input');
     const checked = document.querySelectorAll('#category ul li input:checked');
     const prepare = [];
 
@@ -24,7 +24,7 @@ async function init() {
         console.log(prepare, 'all categories');
         response = await axios.post('/list', data);
     } catch (e) {
-        console.log('communitylistinit',e.message);
+        console.log('communitylistinit', e.message);
     }
 
     test = {
@@ -48,6 +48,7 @@ async function init() {
 
     const paging = document.querySelector('#paging');
 
+    const arrPage2 = []
     for (let i = block + 1; i <= endBlock; i++) {
         const liElement = document.createElement('li');
         const aElement = document.createElement('a');
@@ -56,6 +57,7 @@ async function init() {
         aElement.innerHTML = `[${i}]`;
         liElement.appendChild(aElement);
         paging.appendChild(liElement);
+
     }
     // for (let i = block + 1; i <= endBlock; i++) {
     //     paging.innerHTML = '';
@@ -91,19 +93,19 @@ async function init() {
             case 1:
                 td[1].innerHTML = 'Classic';
                 td[1].style.color = "#A5A5A5";
-            break;
+                break;
             case 2:
                 td[1].innerHTML = 'Musical';
                 td[1].style.color = "#DB6039";
-            break;
+                break;
             case 3:
                 td[1].innerHTML = 'Opera';
                 td[1].style.color = "#64CBE6";
-            break;
+                break;
             case 4:
                 td[1].innerHTML = 'Ballet';
                 td[1].style.color = "#FAE100";
-            break;   
+                break;
         }
         td[2].appendChild(aElement);
         td[3].innerHTML = user_nickname
@@ -114,8 +116,8 @@ async function init() {
     })
 
     /* 체크박스 클릭 이벤트 */
-    for (i = 0; i < check.length; i++) {
-        check[i].addEventListener('click', clickHanlder);
+    for (i = 0; i < checks.length; i++) {
+        checks[i].addEventListener('click', clickHanlder);
     }
 }
 
@@ -133,7 +135,7 @@ async function clickHanlder() {
         };
         console.log(prepare, 'clicked')
         response = await axios.post('/list', data);
-        // return response.data;
+
 
         test = {
             ...response
@@ -147,7 +149,6 @@ async function clickHanlder() {
 
         const totalPage = Math.ceil(totalRows / viewRows);
         const blockBox = Math.ceil(totalPage / pagingBlock);
-        console.log(blockBox);
 
         let page = 1;
         const currentBlock = Math.ceil(page / pagingBlock);
@@ -156,108 +157,122 @@ async function clickHanlder() {
         let endBlock = block + pagingBlock;
         if (endBlock > totalPage) endBlock = totalPage;
 
+        ///여기서 부터 체크 페이징
         const paging = document.querySelector('#paging');
-        const arr = []
-        // for (let i = block + 1; i <= endBlock; i++) {
-        //     paging.innerHTML = '';
-        //     const liElement = document.createElement('li');
-        //     const aElement = document.createElement('a');
-        //     liElement.appendChild(aElement);
-        //     paging.appendChild(liElement);
-        //     pages(i)
-            
-        //     arr.push(i)
-        //     // console.log(arr)
-        //     for (let j = 1; j <= arr.length; j++) {
-        //         aElement.innerHTML = `[${arr}]`
-                
-        //     }
-        // }
-
-        for (let j = 1; j <= arr.length; j++){
-            paging.innerHTML = '';
-            for (let i = block + 1; i <= endBlock; i++) {
-                paging.innerHTML = '';
-                const liElement = document.createElement('li');
-                const aElement = document.createElement('a');
-                
-                aElement.setAttribute(`onClick`, `pages(${i})`);
-            
-                aElement.innerHTML = `[${i}]`;
-    
-                liElement.appendChild(aElement);
-                paging.appendChild(liElement);
-                
-            }
+        arr = []
+        //클릭했을 떄 바로 반영이 아닌 페이징 눌러야 반영....length 비교 >>>
+        //처음나오는 페이징이 고정
+        paging.innerHTML = '';
+        for (let i = block + 1; i <= endBlock; i++) {
+            arr.push(i)
+        }
+        console.log(arr.length)
+        for(let j = 1; j <= arr.length ; j++){
+            // console.log(j)
+            const liElement = document.createElement('li');
+            const aElement = document.createElement('a');
+            aElement.innerHTML = `[${j}]`
+            // arr[j] = aElement
         }
 
-    } catch (e) {
-        console.log('communitylist',e.message);
+
+            // paging.innerHTML = '';
+            // arr.push(i)
+            // console.log(arr.length)
+            // for (let j = 0; j <= arr.length; j++) {
+            //     arr.push(j)
+            //     // console.log(arr)
+            //     const liElement = document.createElement('li');
+            //     const aElement = document.createElement('a');
+            //     aElement.addEventListener('click',pages(`${j})`))
+            // }
+
+
+            // aElement.innerHTML = arr
+            // console.log(aElement)
+            // arr.push(aElement.innerHTML)
+            // for (let j = 0; j <= totalRows/10; j++) {
+            //     arr.push(`[${i}]`)
+            //     arr[j].addEventListener('click',`pages(${j})`);
+            //     aElement.setAttribute(`onClick`, `pages(${j})`);
+
+            // }
+            //     arr.forEach(v=>{
+            //         v.addEventListener('click',`pages(${i})`);
+            // })
+            // aElement.setAttribute(`onClick`, `pages(${i})`);
+
+
+            // liElement.appendChild(aElement);
+            // paging.appendChild(liElement);
+            // }
+        } catch (e) {
+            console.log('communitylist', e.message);
+        }
     }
-}
 
 /* 클릭 핸들러 여기까지 */
 
 async function pages(num) {
-    const response1 = await axios.post('http://localhost:3001/account/management/getuserinfo', null);
-    const { user } = response1.data.result;
-    const user_nickname = user.user_nickname;
+        const response1 = await axios.post('http://localhost:3001/account/management/getuserinfo', null);
+        const { user } = response1.data.result;
+        const user_nickname = user.user_nickname;
 
-    console.log('num', num)
-    const tr = document.querySelector('#communityBoardRow');
-    const value = test.data.result;
+        console.log('num', num)
+        const tr = document.querySelector('#communityBoardRow');
+        const value = test.data.result;
 
-    const aElement = document.createElement('a');
-    aElement.href = '/board/community//view' + value.board_idx;
-    aElement.innerHTML = value.board_subject;
-
-    const viewRows = 10;
-    const Nodes = test.data.result.slice((num - 1) * viewRows, num * viewRows);
-    const tbody = document.querySelector('table > tbody');
-
-
-    let template = '';
-    await Nodes.forEach(v => { });
-    tbody.innerHTML = template;
-
-    await Nodes.forEach(v => {
-        const showCategory = v.show_category_idx
-        const clone = document.importNode(tr.content, true);
-        const td = clone.querySelectorAll('td');
         const aElement = document.createElement('a');
-        aElement.href = '/board/community/view/' + v.board_idx;
-        aElement.innerHTML = v.board_subject;
-        
+        aElement.href = '/board/community//view' + value.board_idx;
+        aElement.innerHTML = value.board_subject;
 
-        td[0].innerHTML = v.board_idx;
-        switch (showCategory) {
-            case 1:
-                td[1].innerHTML = 'Classic';
-                td[1].style.color = "#A5A5A5";
-            break;
-            case 2:
-                td[1].innerHTML = 'Musical';
-                td[1].style.color = "#DB6039";
-            break;
-            case 3:
-                td[1].innerHTML = 'Opera';
-                td[1].style.color = "#64CBE6";
-            break;
-            case 4:
-                td[1].innerHTML = 'Ballet';
-                td[1].style.color = "#FAE100";
-            break;   
-        }
-        td[2].appendChild(aElement);
-        
-        td[3].innerHTML = user_nickname
-        td[4].innerHTML = v.board_date;
-        td[5].innerHTML = v.board_hit;
+        const viewRows = 10;
+        const Nodes = test.data.result.slice((num - 1) * viewRows, num * viewRows);
+        const tbody = document.querySelector('table > tbody');
 
-        const tbody1 = document.querySelector('table > tbody');
-        tbody1.appendChild(clone);
-    })
-}
+
+        let template = '';
+        await Nodes.forEach(v => { });
+        tbody.innerHTML = template;
+
+        await Nodes.forEach(v => {
+            const showCategory = v.show_category_idx
+            const clone = document.importNode(tr.content, true);
+            const td = clone.querySelectorAll('td');
+            const aElement = document.createElement('a');
+            aElement.href = '/board/community/view/' + v.board_idx;
+            aElement.innerHTML = v.board_subject;
+
+
+            td[0].innerHTML = v.board_idx;
+            switch (showCategory) {
+                case 1:
+                    td[1].innerHTML = 'Classic';
+                    td[1].style.color = "#A5A5A5";
+                    break;
+                case 2:
+                    td[1].innerHTML = 'Musical';
+                    td[1].style.color = "#DB6039";
+                    break;
+                case 3:
+                    td[1].innerHTML = 'Opera';
+                    td[1].style.color = "#64CBE6";
+                    break;
+                case 4:
+                    td[1].innerHTML = 'Ballet';
+                    td[1].style.color = "#FAE100";
+                    break;
+            }
+            td[2].appendChild(aElement);
+
+            td[3].innerHTML = user_nickname
+            td[4].innerHTML = v.board_date;
+            td[5].innerHTML = v.board_hit;
+
+            const tbody1 = document.querySelector('table > tbody');
+            tbody1.appendChild(clone);
+        })
+    }
 
 
 
---
+++ page.js
@@ -0,0 +1,16 @@
+const block = 0;
+const endBlock = 7;
+
+arr = []
+
+for (let i = block + 1; i <= endBlock; i++) {
+    arr.push(i)
+    console.log(i)
+    console.log(arr.length)
+}
+console.log(arr)
+
+// for (let j = 0; j <= arr.length; j++) {
+//     arr.push(j)
+
+// }
