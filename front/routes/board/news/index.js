3aafd708bd4e261f9555592d25ddb9a0a5cf9532
jihyun kim <jihyunivy@gmail.com>
2022-04-06 14:16:46 +0900

    commit for merge

--- front/routes/board/news/index.js
+++ front/routes/board/news/index.js
@@ -4,7 +4,7 @@ const newsController = require('./newsController')
 
 router.get('/list',newsController.newsList)
 router.get('/write',newsController.newsWrite)  //어드민만 버튼 보이게 해서 이동할 수 있도록
-router.get('/view:idx',newsController.newsView)
-router.get('/update:idx',newsController.newsUpdate)
+router.get(`/view/:idx`,newsController.newsView)
+router.get(`/update/:idx`,newsController.newsUpdate)
 
 module.exports = router
