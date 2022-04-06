35c86fb27192d208a6f6af776ff49d923bc90f0b
hancoco <lhj12ps@gmail.com>
2022-04-06 10:39:49 +0900

    재재재커밋

--- back/routes/show/program/index.js
+++ back/routes/show/program/index.js
@@ -9,11 +9,7 @@ const multer = require('multer')
 const upload = multer({
     storage : multer.diskStorage({
         destination : (req,file,done)=>{
-<<<<<<< HEAD
-            done(null,'public/uploads/s_uploads')
-=======
-            done(null,'uploads//s_uploads')
->>>>>>> 2e89345828c0c320cdb3779db328a636e1d3b383
+            done(null,'uploads/s_uploads')
         },
         filename:(req,file,done)=>{
             const ext = path.extname(file.originalname)
