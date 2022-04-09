28359e60953edfff53e683d2e43aca8143546c31
jihyun kim <jihyunivy@gmail.com>
2022-04-09 17:25:41 +0900

    done payment_1

--- back/routes/book/book/bookController.js
+++ back/routes/book/book/bookController.js
@@ -1,3 +1,6 @@
+const pool = require('../../../db');
+const sql = require('../../../SQL/queries.js')
+
 exports.selectBookInfo = async (req,res) => {
     let response = {
         result: {},
--- front/public/script/payment/payment_1.js
+++ front/public/script/payment/payment_1.js
@@ -2,16 +2,21 @@ let test = {};
 document.addEventListener('DOMContentLoaded', init);
 
 async function init() {
-    axios.defaults.baseURL = 'http://localhost:4001/board/community';
+    axios.defaults.baseURL = 'http://localhost:4001/book/payment';
     axios.defaults.headers.post['Content-Type'] = 'application/json';
     axios.defaults.withCredentials = true;
 
-    const answer = document.querySelectorAll('.answer input')
-    const yes = document.querySelector('#yes')
-    const pointCheckBox = document.querySelector('#pointCheckBox')
+    const userInfo = await axios.post('http://localhost:3001/account/management/getuserinfo', null);
+    const { user } = userInfo.data.result;
 
-    answer.forEach(v =>v.addEventListener('click', clickHandler))
 
+    const answer = document.querySelectorAll('.answer input');
+    const yes = document.querySelector('#yes');
+    const pointCheckBox = document.querySelector('#pointCheckBox');
+
+    answer.forEach(v =>v.addEventListener('click', clickHandler));
+
+    //클릭 시 포인트 사용 여부 체크
     function clickHandler() {
         if (yes.checked) {
             pointCheckBox.style.display = 'block';
@@ -20,12 +25,36 @@ async function init() {
         }
     }
 
-    pointCheckBtn.addEventListener('click',pointCheckBtnhandler)
-
+    //클릭 시 포인트 조회
+    pointCheckBtn.addEventListener('click',pointCheckBtnhandler);
     async function pointCheckBtnhandler(){
-        const response = await axios.post('')
+        const usablePoint = document.querySelector('#usablePoint');
+        const data = {
+            userIdx: user.user_idx
+        };
+        const response = await axios.post('/checkPoint',data);
+        const point = response.data.result.point_net;
+        usablePoint.innerHTML = point;
+
+    }
+
+    const ticketPrice = document.querySelector('#ticketPrice')
+    const deadLine = document.querySelector('#deadLine')
+    try {
+        // const response = await axios.post('/payment_1',)
+    } catch (e) {
+        console.log('/payment_1',e.message)
+    }
+    ticketPrice.innerHTML = '1000'
+    deadLine.innerHTML = '2022-02-02 PM 23:59'
+    
 
 
 
+    const next = document.querySelector('#next')
+    next.addEventListener('click',moveToPayment_2Hanlder)
+
+    function moveToPayment_2Hanlder(){
+        location.href='/book/payment/payment_2'
     }
 }
